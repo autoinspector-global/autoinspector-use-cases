f383791f1cab6a279998ad916eb183cfc8656fa4
lcnlvrz-revelacion <luciano.a@revelaciondata.com.ar>
2022-07-14 18:10:17 -0300

    update some function calls to the new sdk version

--- digital-insurer/nodejs/main.js
+++ digital-insurer/nodejs/main.js
@@ -1,42 +1,41 @@
-const express = require("express");
-const connectDB = require("./db/connect");
-const availablePolicyEntity = require("./entity/available-policy.entity");
-const customerEntity = require("./entity/customer.entity");
+const express = require('express')
+const connectDB = require('./db/connect')
+const availablePolicyEntity = require('./entity/available-policy.entity')
+const customerEntity = require('./entity/customer.entity')
 
-const policyEntity = require("./entity/policy.entity");
+const policyEntity = require('./entity/policy.entity')
 
-const { MongoMemoryServer } = require("mongodb-memory-server");
+const { MongoMemoryServer } = require('mongodb-memory-server')
 
-require("dotenv").config({
-  path: ".env",
-});
+require('dotenv').config({
+  path: '.env',
+})
 
-const Autoinspector = require("autoinspector");
-const availableGoodEntity = require("./entity/available-good.entity");
-const AvailablePoliciesSeeder = require("./db/seeders/available-policies.seeder");
-const AvailableGoodsSeeder = require("./db/seeders/available-goods.seeder");
-const flow = require("./test/flow");
+const Autoinspector = require('autoinspector')
+const availableGoodEntity = require('./entity/available-good.entity')
+const AvailablePoliciesSeeder = require('./db/seeders/available-policies.seeder')
+const AvailableGoodsSeeder = require('./db/seeders/available-goods.seeder')
 
 // Instantiate autoinspector SDK
 const autoinspector = new Autoinspector({
   apikey: process.env.AUTOINSPECTOR_API_KEY,
-});
+})
 
-const app = express();
+const app = express()
 
 // Here we sent into the request a new key: rawBody. This new key will have the buffer as he cames without parses. This new key will be consumed in the webhook endpoint, at the moment of verify hmac signature.
 app.use(
   express.json({
     verify: (req, res, buf) => {
-      req.rawBody = buf;
+      req.rawBody = buf
     },
   })
-);
+)
 
-app.post("/policy/:availablePolicyId", async (req, res) => {
+app.post('/policy/:availablePolicyId', async (req, res) => {
   const availablePolicy = await availablePolicyEntity.findOne({
     _id: req.params.availablePolicyId,
-  });
+  })
 
   const customer = await customerEntity.create({
     occupation: req.body.customer.occupation,
@@ -44,17 +43,17 @@ app.post("/policy/:availablePolicyId", async (req, res) => {
     lastname: req.body.customer.lastname,
     email: req.body.customer.email,
     identification: req.body.customer.identification,
-  });
+  })
 
   const policy = await policyEntity.create({
     customerId: customer._id,
     availablePolicyId: availablePolicy._id,
-    status: "waiting_verification",
-  });
+    status: 'waiting_verification',
+  })
 
   const inspection = await autoinspector.inspections.goods.create({
     // Initialize the inspection with the started status. With this value, we avoid to start the inspection.
-    initialStatus: "started",
+    initialStatus: 'started',
     delivery: {
       // We disable the delivery of all notifications. By this way, we have full control about how to communicate with our users
       disabled: true,
@@ -74,7 +73,7 @@ app.post("/policy/:availablePolicyId", async (req, res) => {
     },
     // We pass the template id that we created before. Here, we are mapping the environment variables exported from .env file.
     templateId: process.env.AUTOINSPECTOR_CUSTOM_TEMPLATE_ID,
-  });
+  })
 
   // Update the policy entity defining the inspection id. This is a must if we want to associate policy <-> inspection
   await policyEntity.updateOne(
@@ -86,27 +85,17 @@ app.post("/policy/:availablePolicyId", async (req, res) => {
         inspectionId: inspection.inspectionId,
       },
     }
-  );
+  )
 
   res.status(201).json({
     policyId: policy._id,
     inspectionId: inspection.inspectionId,
-  });
-});
+  })
+})
 
-app.get("/test/flow", async (req, res) => {
-  try {
-    await flow();
-    res.status(200).json({ success: true });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send(err);
-  }
-});
-
-app.post("/policy/:policyId/items", async (req, res) => {
+app.post('/policy/:policyId/items', async (req, res) => {
   //Create a list of strings that belongs to available goods ids
-  const availableGoodsIds = req.body.goods.map((good) => good.availableGoodId);
+  const availableGoodsIds = req.body.goods.map(good => good.availableGoodId)
 
   //Make a bulk get query to the database
   const availableGoods = await availableGoodEntity
@@ -115,32 +104,32 @@ app.post("/policy/:policyId/items", async (req, res) => {
         $in: availableGoodsIds,
       },
     })
-    .lean();
+    .lean()
 
   //Prepare the goods array to send to Autoinspector API
   const goodsMerged = availableGoods.map((availableGood, index) => {
-    const goodDetails = req.body.goods[index];
+    const goodDetails = req.body.goods[index]
 
     return {
       ...goodDetails,
       ...availableGood,
-    };
-  });
+    }
+  })
 
   const policy = await policyEntity.findOne({
     _id: req.params.policyId,
-  });
+  })
 
   // Add the goods to the tinspection
-  const goods = await autoinspector.inspections.goods.addGoods(
+  const goods = await autoinspector.inspections.goods.push(
     policy.inspectionId,
     goodsMerged
-  );
+  )
 
   // Prepare the goods array to set into the policy object
   const goodsToPush = availableGoods.map((availableGood, index) => {
-    const productInspectionId = goods.productIds[index];
-    const goodDetails = req.body.goods[index];
+    const productInspectionId = goods.productIds[index]
+    const goodDetails = req.body.goods[index]
 
     return {
       availableGoodId: availableGood._id,
@@ -150,8 +139,8 @@ app.post("/policy/:policyId/items", async (req, res) => {
       make: goodDetails.make,
       model: goodDetails.model,
       price: goodDetails.price,
-    };
-  });
+    }
+  })
 
   // Push into the goods property of the policy entity the goods list built
   const policyUpdate = await policyEntity.findOneAndUpdate(
@@ -166,53 +155,53 @@ app.post("/policy/:policyId/items", async (req, res) => {
     {
       new: true,
     }
-  );
+  )
 
-  res.status(201).json(policyUpdate.goods);
-});
+  res.status(201).json(policyUpdate.goods)
+})
 
 app.post(
-  "/policy/:policyId/goods/:goodId/inspection/image",
+  '/policy/:policyId/goods/:goodId/inspection/image',
   async (req, res) => {
     // Get the policy that good belongs to
     const policy = await policyEntity.findOne({
       _id: req.params.policyId,
-      "goods._id": req.params.goodId,
-    });
+      'goods._id': req.params.goodId,
+    })
 
     const goodPolicy = policy.goods.find(
-      (good) => good._id.toString() === req.params.goodId
-    );
+      good => good._id.toString() === req.params.goodId
+    )
 
     // Generates the image token and return it
     const imageToken = await autoinspector.images.generateToken({
       productId: goodPolicy.productInspectionId,
       side: req.body.side,
       coordinates: req.body.coordinates,
-    });
+    })
 
-    res.status(201).json({ imageToken });
+    res.status(201).json({ imageToken })
   }
-);
+)
 
-app.post("/policy/:policyId/inspection/finish", async (req, res) => {
+app.post('/policy/:policyId/inspection/finish', async (req, res) => {
   const policy = await policyEntity.findOne({
     _id: req.params.policyId,
-  });
+  })
 
   // Complete the inspection
   await autoinspector.inspections.finish({
     inspectionId: policy.inspectionId,
-  });
+  })
 
-  res.status(200).json({ finish: true });
-});
+  res.status(200).json({ finish: true })
+})
 
-app.post("/webhook", async (req, res) => {
+app.post('/webhook', async (req, res) => {
   // This is Autoinspector SHA256 signature to verify if the request body is corrupted and to ensure that who are making the request is Autoinspector API
-  const signature = req.headers["autoinspector-signature"];
+  const signature = req.headers['autoinspector-signature']
 
-  let webhook;
+  let webhook
 
   try {
     //Here we use the autoinspector sdk to handle al the hmac validation. Just pass the req.rawBody that we set at the beginning via middleware, the signature provided from request and the webhook secret generated by Autoinspector for us
@@ -220,75 +209,48 @@ app.post("/webhook", async (req, res) => {
       req.rawBody,
       signature,
       process.env.AUTOINSPECTOR_WEBHOOK_SECRET
-    );
+    )
   } catch (err) {
-    return res.status(400).json({ error: `Webhook error: ${err.message} ` });
+    return res.status(400).json({ error: `Webhook error: ${err.message} ` })
   }
 
   // At this point is safely to map the webhook properties. We know that message is not corrupted and comes from Autoinspector
   switch (webhook.event) {
-    case "inspection_completed":
-      const isInspectionApproved = webhook.payload.veredict === "approved";
+    case 'inspection_completed':
+      //inspection completed
 
-      if (isInspectionApproved) {
-        await policyEntity.updateOne(
-          {
-            _id: webhook.payload.metadata.policyId,
-          },
-          {
-            $set: {
-              status: "issued",
-              startDate: new Date(),
-            },
-          }
-        );
-      }
-
-      if (!isInspectionApproved) {
-        await policyEntity.updateOne(
-          {
-            _id: webhook.payload.metadata.policyId,
-          },
-          {
-            $set: {
-              status: "declined",
-            },
-          }
-        );
-      }
-
-      break;
+      break
 
     default:
-      console.log(`Unhandled autoinspector event: ${webhook.event}`);
+      console.log(`Unhandled autoinspector event: ${webhook.event}`)
   }
 
-  res.status(200).json({ received: true });
-});
+  res.status(200).json({ received: true })
+})
 
-app.get("/available-goods", async (req, res) => {
-  const availableGoods = await availableGoodEntity.find();
+app.get('/available-goods', async (req, res) => {
+  const availableGoods = await availableGoodEntity.find()
 
-  res.status(200).json(availableGoods);
-});
+  res.status(200).json(availableGoods)
+})
 
-app.get("/available-policies", async (req, res) => {
-  const availablePolicy = await availablePolicyEntity.find();
+app.get('/available-policies', async (req, res) => {
+  const availablePolicy = await availablePolicyEntity.find()
 
-  res.status(200).json(availablePolicy);
-});
+  res.status(200).json(availablePolicy)
+})
 
-const PORT = 4848;
+const PORT = 4848
 
 app.listen(PORT, async () => {
-  const mongod = await MongoMemoryServer.create();
-  const uri = mongod.getUri();
+  const mongod = await MongoMemoryServer.create()
+  const uri = mongod.getUri()
 
-  await connectDB(uri);
+  await connectDB(uri)
 
   // A couple of seeders to start with initial data in our database
-  await AvailablePoliciesSeeder.seed();
-  await AvailableGoodsSeeder.seed();
+  await AvailablePoliciesSeeder.seed()
+  await AvailableGoodsSeeder.seed()
 
-  console.log("digital insurer backend listening on:", PORT);
-});
+  console.log('digital insurer backend listening on:', PORT)
+})
--- kyc/nodejs/main.js
+++ kyc/nodejs/main.js
@@ -1,36 +1,34 @@
-const express = require("express");
-const path = require("path");
-const connectDB = require("./db/connect");
+const express = require('express')
+const path = require('path')
+const connectDB = require('./db/connect')
 
-const { MongoMemoryServer } = require("mongodb-memory-server");
+const { MongoMemoryServer } = require('mongodb-memory-server')
 
-require("dotenv").config({
-  path: ".env",
-});
+require('dotenv').config({
+  path: '.env',
+})
 
-const Autoinspector = require("autoinspector");
-const userEntity = require("./entities/user.entity");
+const Autoinspector = require('autoinspector')
+const userEntity = require('./entities/user.entity')
 
 // Instantiate autoinspector SDK
 const autoinspector = new Autoinspector({
   apikey: process.env.AUTOINSPECTOR_API_KEY,
-});
+})
 
-const app = express();
+const app = express()
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get("/identity/verification", async (req, res) => {
+app.get('/identity/verification', async (req, res) => {
   // Here we extract the inspectionId from the request query
-  const { inspectionId } = req.query;
+  const { inspectionId } = req.query
 
   // Find the inspection id to check the veredict and know to who belongs to
-  const inspection = await autoinspector.inspections.retrieve({
-    inspectionId: inspectionId,
-  });
+  const inspection = await autoinspector.inspections.retrieve(inspectionId)
 
-  const isInspectionApproved = inspection.veredict === "approved";
+  const isInspectionApproved = inspection.veredict === 'approved'
 
   if (isInspectionApproved) {
     //If the inspection is approved, we verify the user account
@@ -43,23 +41,21 @@ app.get("/identity/verification", async (req, res) => {
           verified: true,
         },
       }
-    );
+    )
 
     // We send the view to tell to the user that their account is verified
-    return res.sendFile(
-      path.join(__dirname, "public", "identity-success.html")
-    );
+    return res.sendFile(path.join(__dirname, 'public', 'identity-success.html'))
   }
 
   // Otherwise, we send the view to tell to the user that their could not verify their identity
-  return res.sendFile(path.join(__dirname, "public", "identity-declined.html"));
-});
+  return res.sendFile(path.join(__dirname, 'public', 'identity-declined.html'))
+})
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
-app.post("/auth/register", async (req, res) => {
+app.post('/auth/register', async (req, res) => {
   // We create the user
   const user = await userEntity.create({
     email: req.body.email,
@@ -70,12 +66,12 @@ app.post("/auth/register", async (req, res) => {
     password: req.body.password,
     // See that here we start the user as non verified one
     verified: false,
-  });
+  })
 
   const inspection = await autoinspector.inspections.people.create({
     // See here that we pass the callbackURL. Because our server takes care of backend and frontend, we need the user back to our same domain
-    callbackURL: `${req.get("origin")}/identity/verification`,
-    initialStatus: "started",
+    callbackURL: `${req.get('origin')}/identity/verification`,
+    initialStatus: 'started',
     delivery: {
       // We disable the delivery of all notifications. By this way, we avoid to send emails from Autoinspector to our client
       disabled: true,
@@ -95,20 +91,20 @@ app.post("/auth/register", async (req, res) => {
       identification: user.identification,
     },
     //Here we send a built-in template alias. If you want to customize your inspection template, you can do it using inspection studio
-    templateId: "easy",
-  });
+    templateId: 'easy',
+  })
 
   //Here we redirect our user from our domain to Autoinspector Inspection App usin the magicLink property
-  res.status(301).redirect(inspection.magicLink);
-});
+  res.status(301).redirect(inspection.magicLink)
+})
 
-const PORT = 4848;
+const PORT = 4848
 
 app.listen(PORT, async () => {
-  const mongod = await MongoMemoryServer.create();
-  const uri = mongod.getUri();
+  const mongod = await MongoMemoryServer.create()
+  const uri = mongod.getUri()
 
-  await connectDB(uri);
+  await connectDB(uri)
 
-  console.log("kyc backend listening on:", PORT);
-});
+  console.log('kyc backend listening on:', PORT)
+})
