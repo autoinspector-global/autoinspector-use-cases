585ba3ba199afa810c6315b58ebf95c4db022d55
lcnlvrz-revelacion <luciano.a@revelaciondata.com.ar>
2022-07-14 13:00:25 -0300

    kyc - create a new subfolder to separate different language implementations

--- kyc/nodejs/main.js
+++ kyc/nodejs/main.js
@@ -22,8 +22,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/identity/verification", async (req, res) => {
+  // Here we extract the inspectionId from the request query
   const { inspectionId } = req.query;
 
+  // Find the inspection id to check the veredict and know to who belongs to
   const inspection = await autoinspector.inspections.retrieve({
     inspectionId: inspectionId,
   });
@@ -31,6 +33,7 @@ app.get("/identity/verification", async (req, res) => {
   const isInspectionApproved = inspection.veredict === "approved";
 
   if (isInspectionApproved) {
+    //If the inspection is approved, we verify the user account
     await userEntity.updateOne(
       {
         _id: inspection.metadata.userId,
@@ -42,11 +45,13 @@ app.get("/identity/verification", async (req, res) => {
       }
     );
 
+    // We send the view to tell to the user that their account is verified
     return res.sendFile(
       path.join(__dirname, "public", "identity-success.html")
     );
   }
 
+  // Otherwise, we send the view to tell to the user that their could not verify their identity
   return res.sendFile(path.join(__dirname, "public", "identity-declined.html"));
 });
 
@@ -55,6 +60,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/auth/register", async (req, res) => {
+  // We create the user
   const user = await userEntity.create({
     email: req.body.email,
     firstname: req.body.firstname,
@@ -62,15 +68,16 @@ app.post("/auth/register", async (req, res) => {
     identification: req.body.identification,
     username: req.body.username,
     password: req.body.password,
+    // See that here we start the user as non verified one
     verified: false,
   });
 
   const inspection = await autoinspector.inspections.people.create({
-    // Initialize the inspection with the started status. With this value, we avoid to start the inspection.
+    // See here that we pass the callbackURL. Because our server takes care of backend and frontend, we need the user back to our same domain
     callbackURL: `${req.get("origin")}/identity/verification`,
     initialStatus: "started",
     delivery: {
-      // We disable the delivery of all notifications. By this way, we have full control about how to communicate with our users
+      // We disable the delivery of all notifications. By this way, we avoid to send emails from Autoinspector to our client
       disabled: true,
     },
     metadata: {
@@ -80,16 +87,18 @@ app.post("/auth/register", async (req, res) => {
     producer: {
       internalId: user._id,
     },
+    // Here we pass all the information related to our user registered
     consumer: {
       email: user.email,
       firstName: user.firstname,
       lastName: user.lastname,
       identification: user.identification,
     },
-    //Here we send built-in template alias mapping, so we don't need to send an unique template identifier.
+    //Here we send a built-in template alias. If you want to customize your inspection template, you can do it using inspection studio
     templateId: "easy",
   });
 
+  //Here we redirect our user from our domain to Autoinspector Inspection App usin the magicLink property
   res.status(301).redirect(inspection.magicLink);
 });
 
